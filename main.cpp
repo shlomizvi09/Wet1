6e9d7984189ebe6369a8836f82c46d00f9384191
shlomizvi09 <54794060+shlomizvi09@users.noreply.github.com>
2020-05-08 19:44:03 +0300

    finished implementing TreeNode

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <stdbool.h>
+#include "AVLtree.h"
+#include <string>
+using std::string;
+
+class Person {
+  string name;
+  int age;
+ public:
+  Person() : name(), age() {}
+  Person(string name, int age) : name(name), age(age) {}
+  Person(const Person &Old) : name(Old.name), age(Old.age) {}
+  friend std::ostream &operator<<(std::ostream &os, const Person &other);
+};
+
+std::ostream &operator<<(std::ostream &os, const Person &other) {
+  os << other.age << " ";
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Person son("shlomi", 26);
   return 0;
 }
