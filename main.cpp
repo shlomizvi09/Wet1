b2d967391cc4eac9e403d2020f49b1b473f33b8c
Arik <berenshtein@campus.technion.ac.il>
2020-05-09 16:07:07 +0300

    added linked list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include "LinkedList.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    LinkedList<int> arik;
+    LinkedList<int>::Node a = arik.insertInPlace(arik.getHead(), 1);
+    LinkedList<int>::Node b = arik.insertInPlace(&a, 2);
+    LinkedList<int>::Node c = arik.insertInPlace(&b, 3);
+    std::cout << "Hello, World!" << std::endl;
+
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
