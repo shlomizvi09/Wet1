b91d0ab57e1425aaaba946a809e0d8ce6139c62b
Arik <berenshtein@campus.technion.ac.il>
2020-05-16 19:22:25 +0300

    finish all

--- main.cpp
+++ main.cpp
@@ -76,19 +76,33 @@ int main() {
     for (int i = 0; i < 20; ++i) {
         AddToSongCount(music_manager, 1, 4);
     }
-    AddArtist(music_manager, 2, 1);
-    AddArtist(music_manager, 4, 1);
-    AddArtist(music_manager, 5, 1);
-    AddArtist(music_manager, 8, 1);
-    AddArtist(music_manager, 9, 1);
+    AddArtist(music_manager, 3, 10);
+    AddArtist(music_manager, 2, 10);
+    AddArtist(music_manager, 5, 10);
+    AddArtist(music_manager, 8, 10);
+    AddArtist(music_manager, 9, 10);
 
     AddToSongCount(music_manager, 2, 0);
     RemoveArtist(music_manager, 1);
-    AddToSongCount(music_manager, 2, 0);
-    AddToSongCount(music_manager, 2, 0);
-    AddToSongCount(music_manager, 2, 0);
-    AddToSongCount(music_manager, 2, 0);
-    AddToSongCount(music_manager, 2, 0);
+    AddToSongCount(music_manager, 2, 6);
+    AddToSongCount(music_manager, 3, 6);
+    AddToSongCount(music_manager, 3, 6);
+    AddToSongCount(music_manager, 3, 5);
+    AddToSongCount(music_manager, 8, 3);
+    AddToSongCount(music_manager, 9, 4);
+    AddToSongCount(music_manager, 9, 4);
+    AddToSongCount(music_manager, 9, 3);
+    AddToSongCount(music_manager, 2, 3);
+    AddToSongCount(music_manager, 9, 2);
+    AddToSongCount(music_manager, 5, 2);
+    AddToSongCount(music_manager, 5, 2);
+    AddToSongCount(music_manager, 3, 1);
+
+
+    int artist[3];
+    int songs[3];
+
+    GetRecommendedSongs(music_manager,3,artist,songs);
     RemoveArtist(music_manager, 2);
     RemoveArtist(music_manager, 4);
     RemoveArtist(music_manager, 5);
